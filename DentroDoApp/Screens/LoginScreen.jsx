35111104a3509de3bfc6ebc3e95d0c1dd0def9c2
RafaelFigueiredoCode <rafaelfigueira203@gmail.com>
2025-08-27 11:28:30 -0300

    adicionei umas coisas a mais antes de terminar a aula

---
+++ DentroDoApp/Screens/LoginScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { useState, useEffect } from 'react';
+import CustomButton from '../components/CustomButton';
+import axios from 'axios';
+
+export default LoginScreen (){
+
+    return (
+         <View>
+         
+         
+         </View>
+
+    )
+}
